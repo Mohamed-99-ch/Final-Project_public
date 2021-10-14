a1fcc899c229c67e8f2bf9f63ccbe7f11d08a81d
Mohamed Fathy <75922327+Mohamed-99-ch@users.noreply.github.com>
2021-10-14 20:33:42 +0200

    Add files via upload

---
+++ add product page.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="add product page_style.css">
+    <title>Add Product Page</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="title">Add a product</div>
+
+        <div class="detales">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis id sed aliquid ipsa, commodi necessitatibus. Modi sit nulla obcaecati rerum ea laborum, iusto iure. Error autem debitis veniam earum quos!
+        </div>
+
+        <div class="product_name">
+            <span class="pro_name">Product Name</span><br>
+            <input type="text" class="pro_name_input" placeholder="Product Name"><br>
+            <span class="category">Category</span><br>
+            <input type="text" class="Category_name_input" placeholder="Category"><br>
+            <span class="brand">Brand</span><br>
+            <input type="text" class="brand_input" placeholder="Brand"><br>
+            <span class="desc">Description</span><br>
+            <textarea name="" id="" cols="50" rows="10" placeholder="Please write a short Description for The Product" class="desc_textarea"></textarea> 
+        </div>
+
+        <div class="product_imag">
+            <span class="product_img">Product Image</span>
+            <div class="pro_img"></div><br>
+            <div class="pro_detales">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptate architecto commodi aspernatur tempore officiis laborum impedit soluta dolor enim iusto distinctio vitae dolorum dignissimos, similique ex deleniti explicabo temporibus!
+            </div><br>
+            <span class="pro_size">Add Size</span><br>
+            <input type="number" class="pro_size_input"><br>
+            <span class="pro_date">Product Date</span><br>
+            <input type="date" class="pro_date_input"><br>
+            <input type="submit" value="Add Product" class="add_button">
+        </div>
+    </div>
+</body>
+
+</html>
